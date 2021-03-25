d2a34a356119da68635318b48a70be627819ab9b
Karthik Chennupati <edu.karthikch@gmail.com>
2021-03-25 12:03:32 +0530

    Project Setup

---
+++ src/main/java/crio/vicara/VicaraT7Application.java
@@ -0,0 +1,12 @@
+package crio.vicara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicaraT7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VicaraT7Application.class, args);
+	}
+}
---
+++ src/test/java/crio/vicara/VicaraT7ApplicationTests.java
@@ -0,0 +1,13 @@
+package crio.vicara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VicaraT7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
